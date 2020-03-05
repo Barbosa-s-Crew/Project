9533f6d6853e96f9958f8a0d2d120ebc55812ace
Narek Zamanyan <narekzamanyan97@gmail.com>
2020-03-04 19:17:35 -0800

    Template Update

--- Django Project/project/dumpApp/views.py
+++ Django Project/project/dumpApp/views.py
@@ -4,8 +4,34 @@ from . import sktest
 
 posts =  sktest.getDB()
 
+deals = [
+	{
+		'name': 'Deal 1',
+		'food_type': 'Deal Type 1',
+		'restaurant': 'Restaurant 1',
+		'price': '$11',
+		'rating': '4.5 Stars',
+		'is_saved': 'false',
+		'duration': '20-30 min',
+		'items': 'item_1'
+	},
+	{
+		'name': 'Deal 2',
+		'food_type': 'Deal Type 2',
+		'restaurant': 'Restaurant 2',
+		'price': '$22',
+		'rating': '3 Stars',
+		'ss_saved': 'true',
+		'duration': '30-35 min',
+		'items': 'item_2'
+		}
+]
+
 def home(request):
-	return render(request, 'dumpApp/home.html')
+	context_deals = {
+		'deals': deals
+	}
+	return render(request, 'dumpApp/home.html', context_deals)
 
 def dump(request):
 	context = {
@@ -14,6 +40,4 @@ def dump(request):
 	return render(request, 'dumpApp/dump.html', context)
 
 def about(request):
-	return render(request, 'dumpApp/about.html')
-
-
+	return render(request, 'dumpApp/about.html', {'title': 'About'})
