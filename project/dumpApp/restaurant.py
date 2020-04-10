18dcf64f5f197b71883ec3fe432ca044961b448d
Hunter <jikemsa@gmail.com>
2020-04-09 23:23:50 -0700

    replaced from . in from . import DBSetup and commented out a test function.

--- project/dumpApp/restaurant.py
+++ project/dumpApp/restaurant.py
@@ -135,4 +135,4 @@ def get_favorites_using_user_ID(ID=0):	#using dumb query, needs updating
 #get_restaurant_using_keyword('pizza')
 #get_menu_items_using_restaurant_ID(9)
 #print(get_recent_using_user_ID(1))
-print(get_favorites_using_user_ID(1))
+#print(get_favorites_using_user_ID(1))
