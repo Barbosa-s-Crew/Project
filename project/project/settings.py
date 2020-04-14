56175fb51706d141cf381f2db12b5b4fb7587150
Narek Zamanyan <narekzamanyan97@gmail.com>
2020-04-14 14:21:47 -0700

    added static folder

--- project/project/settings.py
+++ project/project/settings.py
@@ -122,4 +122,24 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = 'dumpApp/static/'
+
+#added
+# BASE_DIR = os.path.dirname(os.path.dirname(__file__))
+
+# STATIC_URL = '/static/'
+
+# STATIC_ROOT = os.path.join(BASE_DIR, '../static')
+
+
+STATICFILES_DIR = [
+    os.path.join(BASE_DIR, 'static'),
+    #"/project/dumpApp/static"
+]
+# STATICFILES_FINDERS = (
+# 'django.contrib.staticfiles.finders.AppDirectoriesFinder',
+# 'django.contrib.staticfiles.finders.FileSystemFinder',
+# )
+#added
+
+#print ("base dir path", os.path.join(BASE_DIR, 'dumpApp\static\dumpApp'))
