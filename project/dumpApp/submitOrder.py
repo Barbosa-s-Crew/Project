64f162b3d9ce3198a174dea735169a0de6fca577
Hunter <jikemsa@gmail.com>
2020-04-15 19:24:12 -0700

    reworked getOrder function to return an object instead of a list of dictionaries

--- project/dumpApp/submitOrder.py
+++ project/dumpApp/submitOrder.py
@@ -81,28 +81,28 @@ class order_list:
 			return order_ID[0][0]
 
 
-	def getOrder(order_ID):
-		# Obtain connection string information from the portal
-		config = DBSetup.setup_config()
-		try:
-			conn = mysql.connector.connect(**config)
-			print("Connection established")
-		except mysql.connector.Error as err:
-			if err.errno == errorcode.ER_ACCESS_DENIED_ERROR:
-				print("Something is wrong with the user name or password")
-			elif err.errno == errorcode.ER_BAD_DB_ERROR:
-				print("Database does not exist")
-			else:
-				print(err)
-		else: 
-			cursor = conn.cursor()
-			cursor.execute("SELECT * FROM Orders WHERE Order_ID='"+str(order_ID)+"'")
-			fetched = cursor.fetchall()
-			order = fetch[0]
-			ret = list()
-			ret.append(dict(Order_ID=str(order[0]),User_ID=str(order[1]),Location_ID=str(order[2]),Order_start_time=str(order[3]),Order_end_time=str(order[4]),Order_status=str(order[5])))
-			cursor.execute("SELECT * FROM Order_items WHERE Order_ID='"+str(order_ID)+"'")
-			fetched = cursor.fetchall()
-			for order in fetched:
-				ret.append(dict(Order_ID=str(order[0]), Restaurant_ID=str(order[1]), Menu_ID=str(order[2]), Item_ID=str(order[3]), Item_Quantity=str(order[4])))
-			return ret
+def getOrder(order_ID):
+	# Obtain connection string information from the portal
+	config = DBSetup.setup_config()
+	try:
+		conn = mysql.connector.connect(**config)
+		print("Connection established")
+	except mysql.connector.Error as err:
+		if err.errno == errorcode.ER_ACCESS_DENIED_ERROR:
+			print("Something is wrong with the user name or password")
+		elif err.errno == errorcode.ER_BAD_DB_ERROR:
+			print("Database does not exist")
+		else:
+			print(err)
+	else: 
+		cursor = conn.cursor()
+		cursor.execute("SELECT * FROM Orders WHERE Order_ID='"+str(order_ID)+"'")
+		fetched = cursor.fetchall()
+		order = fetch[0]
+		#Full string from the query:Order_ID=str(order[0]),User_ID=str(order[1]),Location_ID=str(order[2]),Order_start_time=str(order[3]),Order_end_time=str(order[4]),Order_status=str(order[5])
+		ret = order_list(order[1],order[2],order[5])
+		cursor.execute("SELECT * FROM Order_items WHERE Order_ID='"+str(order_ID)+"'")
+		fetched = cursor.fetchall()
+		for order in fetched:
+			ret.olist.append(dict(Order_ID=str(order[0]), Restaurant_ID=str(order[1]), Menu_ID=str(order[2]), Item_ID=str(order[3]), Item_Quantity=str(order[4])))
+		return ret
